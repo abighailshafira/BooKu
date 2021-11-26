021627f9b985f59b5afabf97819c7090ac50a31d
Abighail <abighailshafira@upi.edu>
2021-11-26 13:47:54 +0700

    Merapikan kode sesuai style guide airbnb

--- BooKu/webpack.common.js
+++ BooKu/webpack.common.js
@@ -1,46 +1,60 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+/* eslint-disable linebreak-style */
+/* eslint-disable import/no-extraneous-dependencies */
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src/scripts/index.js'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader"
-                    }
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/,
-                use: [
-                    'file-loader',
-                ],
-            },
+  entry: path.resolve(__dirname, 'src/scripts/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src/templates/index.html'),
-            filename: 'index.html',
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, 'src/public/'),
-                    to: path.resolve(__dirname, 'dist/')
-                },
-            ],
-        }),
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/templates/index.html'),
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/templates/login.html'),
+      filename: 'login.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/templates/register.html'),
+      filename: 'register.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/templates/profile.html'),
+      filename: 'profile.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/public/'),
+          to: path.resolve(__dirname, 'dist/'),
+        },
+      ],
+    }),
+  ],
 };
