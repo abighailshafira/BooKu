fb5cf47692b56899abede3a9995b4b3c5e84cf93
Abighail <abighailshafira@upi.edu>
2021-11-30 14:56:59 +0700

    Import custom element

--- BooKu/src/scripts/index.js
+++ BooKu/src/scripts/index.js
@@ -2,3 +2,4 @@ import 'regenerator-runtime';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/style.css';
 import '../styles/responsive.css';
+import '../components/navigation-bar.js';
