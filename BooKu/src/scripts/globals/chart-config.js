4dbb4523373e04feafb64936140a6d4dfd51b535
aaqromrnh <56854566+AARNH@users.noreply.github.com>
2021-12-04 22:53:08 +0700

    Add files via upload

---
+++ BooKu/src/scripts/globals/chart-config.js
@@ -0,0 +1,14 @@
+/* eslint-disable linebreak-style */
+const configChart = {
+  type: 'line',
+  data: {
+    labels: ['January', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'],
+    datasets: [{
+      label: 'Total Penghasilan Per- bulan',
+      data: [2000000, 2200000, 200000, 4000000, 5000000],
+      borderColor: '#3d405b',
+    }],
+  },
+};
+
+export default configChart;
