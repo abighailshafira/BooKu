beb6f78028bf86d090fc9d2685bc7c271938dab4
aaqromrnh <56854566+AARNH@users.noreply.github.com>
2021-12-04 22:46:24 +0700

    Update index.js

--- BooKu/src/scripts/index.js
+++ BooKu/src/scripts/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 /* eslint-disable import/extensions */
 import 'regenerator-runtime';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -5,3 +7,17 @@ import '../styles/style.css';
 import '../styles/responsive.css';
 import '../components/navigation-bar.js';
 import '../components/skip-to-content.js';
+import App from './views/app.js';
+
+const app = new App({
+  drawer: document.querySelector('#navigationMenu'),
+  content: document.querySelector('#mainContent'),
+});
+
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
+
+window.addEventListener('load', () => {
+  app.renderPage();
+});
