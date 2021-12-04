1b0428f3fdc64d163d52a2eaa767d6341c435a3a
aaqromrnh <56854566+AARNH@users.noreply.github.com>
2021-12-04 22:54:27 +0700

    Add files via upload

---
+++ BooKu/src/scripts/routes/routes.js
@@ -0,0 +1,12 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/extensions */
+import HomePage from '../views/pages/home-page.js';
+import InputPage from '../views/pages/input-page.js';
+
+const routes = {
+  '/': HomePage,
+  '/home': HomePage,
+  '/inputpage': InputPage,
+};
+
+export default routes;
