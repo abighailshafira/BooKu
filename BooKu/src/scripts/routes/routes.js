1906327d84a45080b5508fd6657627a69b40138a
aaqromrnh <56854566+AARNH@users.noreply.github.com>
2021-12-16 14:15:25 +0700

    Update routes.js

--- BooKu/src/scripts/routes/routes.js
+++ BooKu/src/scripts/routes/routes.js
@@ -2,11 +2,13 @@
 /* eslint-disable import/extensions */
 import HomePage from '../views/pages/home-page.js';
 import InputPage from '../views/pages/input-page.js';
+import NotePage from '../views/pages/note-page.js';
 
 const routes = {
   '/': HomePage,
   '/home': HomePage,
   '/inputpage': InputPage,
+  '/notepage': NotePage,
 };
 
 export default routes;
