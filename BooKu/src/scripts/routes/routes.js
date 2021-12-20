b6438f3b861d938b64099582394129dfa7055fe3
Abighail <abighailshafira@upi.edu>
2021-12-20 10:34:35 +0700

    Change routing files

--- BooKu/src/scripts/routes/routes.js
+++ BooKu/src/scripts/routes/routes.js
@@ -1,20 +1,18 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable import/extensions */
-import Login from '../views/pages/login-page.js';
-// import Detail from '../views/pages/detail';
-import Register from '../views/pages/register-page.js';
+import LoginPage from '../views/pages/login-page.js';
+import RegisterPage from '../views/pages/register-page.js';
 import HomePage from '../views/pages/home-page.js';
 import InputPage from '../views/pages/input-page.js';
 import NotePage from '../views/pages/note-page.js';
 
 const routes = {
-  '/': Login,
-  '/register': Register,
-  // '/delete/transaksi/': DeleteTransaksi,
+  '/': LoginPage,
+  '/register': RegisterPage,
   '/home': HomePage,
   '/inputpage': InputPage,
   '/notepage': NotePage,
-  '/detail/:id': Login,
+  '/detail/:id': LoginPage,
 };
 
 export default routes;
